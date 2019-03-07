a97fd35443a57220b53bccff10fe38dc945002d6
shikdernyc <shikdernyc@gmail.com>
2019-03-07 02:34:25 -0500

    ADded root

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from .imageClassifier import ImageClassifier
+from .root import Root
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_bp)
 
 api.add_resource(ImageClassifier, '/classify')
+api.add_resource(Root, '/')
