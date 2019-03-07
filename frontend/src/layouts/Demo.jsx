0ea2142df7ad82dccf69548e8b1f4ce3ccd6e1e5
shikdernyc <shikdernyc@gmail.com>
2019-03-06 23:10:09 -0500

    Removed Top navbar

--- frontend/src/layouts/Demo.jsx
+++ frontend/src/layouts/Demo.jsx
@@ -105,11 +105,11 @@ class Dashboard extends React.Component {
           {...rest}
         />
         <div className={classes.mainPanel} ref="mainPanel">
-          <Navbar
+          {/* <Navbar
             routes={fullRoutes}
             handleDrawerToggle={this.handleDrawerToggle}
             {...rest}
-          />
+          /> */}
           {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
           {this.getRoute() ? (
             <div className={classes.content}>
