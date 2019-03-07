7565dffb3d8eeeba3da78c3cf3219de74a4a4139
shikdernyc <shikdernyc@gmail.com>
2019-03-07 00:53:35 -0500

    Changed setup to run. Taking in config option on create app

---
+++ backend/run.py
@@ -0,0 +1,8 @@
+import os
+from app import create_app
+
+app = create_app(os.getenv('CONFIG') or 'default')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT") or 8082)
--- backend/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app()
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8082)
