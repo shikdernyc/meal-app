571d25ab8c96926439e32e3a067a5b8b5f2b78b9
shikdernyc <shikdernyc@gmail.com>
2019-03-07 02:44:55 -0500

    Added flask cors for api

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from flask_restful import Api
 from .imageClassifier import ImageClassifier
 from .root import Root
+from flask_cors import CORS
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
+CORS(api_bp)
 api = Api(api_bp)
 
 api.add_resource(ImageClassifier, '/classify')
