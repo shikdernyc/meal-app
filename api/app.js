b3c4bd656ce2613e8d7154068a53db00688a21e2
Asif Shikder <shikdernyc@gmail.com>
2019-04-21 23:41:20 -0400

    Connecting to db on start. Added api prefix to routes

--- api/app.js
+++ api/app.js
@@ -1,20 +1,32 @@
+require('dotenv').config();
 let express = require('express');
 let bodyParser = require('body-parser');
 let router = require('./routes');
 const errorHandler = require('./handlers/error');
+const {connectToDB} = require('./models')
+
+const PORT = process.env.PORT || 8000;
 
 let app = express();
-const PORT = process.env.PORT || 8000;
+
+connectToDB().then(()=>{
+  console.log("Successfully connected to Database")
+}).catch(error=>{
+  console.log("Unable to connect to database")
+  console.log(error)
+});
+
 
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
-
 app.use(bodyParser.json());
-app.use(router);
+
+app.use("/api", router);
 app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
