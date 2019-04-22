0350ea9fb85ed4e97331a26e49f8fc845649b942
Asif Shikder <shikdernyc@gmail.com>
2019-04-21 23:40:57 -0400

    Added connectToDB function

--- api/models/index.js
+++ api/models/index.js
@@ -1,32 +1,38 @@
-let envPath = __dirname.substr(0, lastIndexOf('/'));
+let envPath = __dirname.substr(0, __dirname.lastIndexOf('/'));
 require('dotenv').config({ path: `${envPath}/.env` });
 
 const mongoose = require('mongoose');
 mongoose.set('debug', process.env.DEBUG || true);
 mongoose.Promise = Promise;
 
-const connect = () => {
-  mongoose.connect(process.env.MONGODB_URI, {
-    keepAlive: true,
-    useNewUrlParser: true,
-    autoReconnect: true,
-    reconnectInterval: 1000
-  });
-};
-
-if (process.env.NODE_ENV !== 'test') {
-  let reconnect = setInterval(connect, 1000);
-
-  mongoose.connection.on('error', err => {
-    console.log(err);
-  });
-
-  mongoose.connection.once('open', function() {
-    console.log('db connected');
-    clearInterval(reconnect);
-  });
+function connectToDB(){
+  return new Promise((resolve, reject)=>{
+    const connect = () =>{
+      mongoose.connect(process.env.MONGODB_URI, {
+        keepAlive: true,
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectInterval: 1000
+      });
+    };
+    
+    if (process.env.NODE_ENV !== 'test') {
+      let reconnect = setInterval(connect, 1000);
+    
+      mongoose.connection.on('error', err => {
+        reject(err)
+      });
+    
+      mongoose.connection.once('open', function() {
+        // console.log('db connected');
+        clearInterval(reconnect);
+        resolve();
+      });
+    }
+  })
 }
 
 module.exports = {
-  User: require('./User')
+  connectToDB,
+  Food: require('./Food')
 };
