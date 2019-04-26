72b51f7a699853f5d68701812d7a27788ec69455
Asif Shikder <shikdernyc@gmail.com>
2019-04-26 17:39:52 -0400

    Added sqlalchemy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,12 +3,18 @@ import os
 from config import config
 # from .api.imageClassifier import ImageClassifier
 from .api import api_bp
-
+from .models import db
 
 def create_app(config_type):
     app = Flask(__name__)
     app.config.from_object(config[config_type])
 
+    db.init_app(app)
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     app.register_blueprint(api_bp)
+
 
     return app
