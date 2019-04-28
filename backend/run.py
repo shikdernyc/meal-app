3d6596244e0b9b1786e3fc09e61d1579a43fd4b9
Asif Shikder <shikdernyc@gmail.com>
2019-04-28 18:01:01 -0400

    Updated imports

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,7 @@
 import os
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.getenv('CONFIG') or 'default')
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':        
     app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT") or 8082)
