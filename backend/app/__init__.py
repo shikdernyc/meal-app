171803eee08e983d5f4b6a3ab7c739d595f6542a
Asif Shikder <shikdernyc@gmail.com>
2019-04-28 17:59:12 -0400

    Added jwt

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, Blueprint
 import os
 from config import config
-# from .api.imageClassifier import ImageClassifier
+from flask_jwt import JWT
 from .api import api_bp
 from .models import db
+from .security import authenticate, identity
 
 def create_app(config_type):
     app = Flask(__name__)
@@ -13,6 +14,8 @@ def create_app(config_type):
     @app.before_first_request
     def create_tables():
         db.create_all()
+
+    jwt = JWT(app, authenticate, identity)
 
     app.register_blueprint(api_bp)
 
